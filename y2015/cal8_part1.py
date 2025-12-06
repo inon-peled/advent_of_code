1ed7253eb41a530a199170524354db11b7ccb89c
Inon Peled <inon.peled@gmail.com>
2025-12-06 12:39:09 +0200

    y2015 d8

---
+++ y2015/cal8_part1.py
@@ -0,0 +1,38 @@
+from ast import literal_eval
+
+
+def _read_input(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    lines_clean = [l.rstrip() for l in lines]
+    return lines_clean
+
+def _process_one_string(s):
+    len_string_code = len(s)
+    s_in_mem = literal_eval(s)
+    len_in_mem = len(s_in_mem)
+    diff = len_string_code - len_in_mem
+    return diff
+
+def _solve(lines_clean):
+    total_diff = 0
+    for l in lines_clean:
+        total_diff += _process_one_string(l)
+    return total_diff
+
+
+def main(filename):
+    lines_clean = _read_input(filename)
+    solution = _solve(lines_clean)
+    return solution
+
+
+def _test(test_filename):
+    got = main(test_filename)
+    assert got == 12
+    print('\n----- Test passed -----')
+
+
+if __name__ == '__main__':
+    _test('test_data_c8.txt')
+    print(main('data_c8.txt'))

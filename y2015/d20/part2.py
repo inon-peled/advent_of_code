9ec2e693dbffb6fa886e5b576f94b3d6e91aa7a7
Inon Peled <inon.peled@gmail.com>
2025-12-06 23:46:17 +0200

    y2015 d20 part2

--- y2015/d20/part2.py
+++ y2015/d20/part2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def _factorize(num):
     factors = set()
     for i in range(1, int(num ** 0.5) + 1):
@@ -7,39 +10,28 @@ def _factorize(num):
     return factors
 
 
-def _count_gifts(house_number):
+def _count_gifts(elf_visits, house_number):
     factors = _factorize(house_number)
-    num_gifts = 10 * sum(factors)
+    num_gifts = 0
+    for f in factors:
+        if elf_visits[f] <= 50:
+            num_gifts += 11 * f
+            elf_visits[f] += 1
     return num_gifts
-
-
-def _test():
-    cases = [[1, 10],
-             [2, 30],
-             [3, 40],
-             [4, 70],
-             [5, 60],
-             [6, 120],
-             [7, 80],
-             [8, 150],
-             [9, 130]]
-    for num, expected in cases:
-        got = _count_gifts(num)
-        assert got == expected
 
 
 def main(min_gifts):
     i = 0
+    elf_visits = defaultdict(int)
     while True:
         i += 1
         if i % (10 ** 5) == 0:
             print(f'Processed {i:,} numbers so far...')
-        gifts = _count_gifts(i)
+        gifts = _count_gifts(elf_visits, i)
         if gifts >= min_gifts:
             print(f'\nAnswer: {i}')
             break
 
 
 if __name__ == '__main__':
-    _test()
     main(29000000)

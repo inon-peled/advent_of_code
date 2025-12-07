8810c09f9eec807b73832777fabbe7de812b86fd
Inon Peled <inon.peled@gmail.com>
2025-12-07 13:36:33 +0200

    y2015 d13

---
+++ y2015/d13/part2.py
@@ -0,0 +1,47 @@
+from itertools import permutations
+from collections import defaultdict
+
+
+def read_and_parse(filename):
+    lines = open(filename).readlines()
+    instructions = defaultdict(dict)
+    for line in lines:
+        s = line.split()
+        u = s[0]
+        sign = -1 if s[2] == 'lose' else 1
+        change = int(s[3])
+        v = s[-1][:-1]
+        instructions[u][v] = sign * change
+
+    for k in instructions:
+        instructions[k]['you'] = 0
+
+    instructions['you'] = {k: 0 for k in instructions.keys()}
+
+    return instructions
+
+
+def _calc_one_perm(p, instructions):
+    total = 0
+    n = len(p)
+    for i in range(len(p)):
+        curr = p[i]
+        prev = p[(i - 1) % n]
+        next = p[(i + 1) % n]
+        total += instructions[curr][next]
+        total += instructions[curr][prev]
+    return total
+
+
+def solve(instructions):
+    best_total = None
+    for p in permutations(instructions.keys()):
+        curr = _calc_one_perm(p, instructions)
+        if best_total is None or curr > best_total:
+            best_total = curr
+    return best_total
+
+
+if __name__ == '__main__':
+    print()
+    print(solve(read_and_parse('data.txt')))

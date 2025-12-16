dd4255e6f6b4b1ae5c5c6d494f7f7e60cb246719
Inon Peled <inon.peled@pendo.io>
2025-12-16 20:23:21 +0200

    y2016 d19 part1

--- y2016/d18/part1.py
+++ y2016/d18/part1.py
@@ -1,23 +1,35 @@
-def _find_next(state, i, n):
-    j = (i + 1) % n
-    while j not in state:
-        j = (j + 1) % n
-    return j
+DATA = '^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^'
 
 
-def solve(n):
-    state = {e: 1 for e in range(n)}
-    i = 0
+def _next_row(prev):
+    n = len(prev)
+    curr = ''
+    for j in range(n):
+        left = prev[j - 1] if j > 0 else '.'
+        center = prev[j]
+        right = prev[j + 1] if j < n - 1 else '.'
+        trap_cond1 = (left == center == '^' and right == '.')
+        trap_cond2 = (center == right == '^' and left == '.')
+        trap_cond3 = (center == right == '.' and left == '^')
+        trap_cond4 = (center == left == '.' and right == '^')
+        trap_cond_total = trap_cond1 or trap_cond2 or trap_cond3 or trap_cond4
+        tile = '^' if trap_cond_total else '.'
+        curr += tile
+    return curr
 
-    while True:
-        if len(state) == 1:
-            return 1 + list(state)[0]
-        next_i = _find_next(state, i, n)
-        state[i] += state[next_i]
-        state.pop(next_i)
-        i = _find_next(state, next_i, n)
+
+def solve(data, total_rows):
+    prev = data[:]
+    total_safe = prev.count('.')
+    for i in range(1, total_rows):
+        curr = _next_row(prev)
+        total_safe += curr.count('.')
+        prev = curr
+    return total_safe
 
 
 if __name__ == '__main__':
-    assert 3 == solve(5)
-    print('Part 1 solution is:', solve(3018458))
+    assert 6 == solve('..^^.', 3)
+    assert 38 == solve('.^^.^.^^^^', 10)
+    print('Part 1 solution:', solve(DATA, 40))
+    print('Part 2 solution:', solve(DATA, 400000))

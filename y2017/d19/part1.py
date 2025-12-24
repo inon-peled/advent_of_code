21d7f80210a372b72b40c9a7fce7faec15e26425
Inon Peled <inon.peled@gmail.com>
2025-12-24 22:51:00 +0200

    y2017 d17 both parts

---
+++ y2017/d19/part1.py
@@ -0,0 +1,53 @@
+def _parse(fname):
+    with open(fname) as f:
+        mat = [[e for e in l if e not in ('\r', '\n')] for l in f]
+    assert all(len(r) == len(mat[0]) for r in mat)
+
+    pad_row = [' '] * (len(mat[0]) + 2)
+    padded_mat = [pad_row] + [[' '] + r + [' '] for r in mat] + [pad_row]
+    assert all(len(r) == len(padded_mat[0]) for r in padded_mat)
+    return padded_mat
+
+
+def _to_ints(cmplx):
+    return int(cmplx.real), int(cmplx.imag)
+
+
+def _new_direction(mat, direction, i, j):
+    d_i, d_j = _to_ints(direction)
+    if d_i != 0:
+        new_dir = complex(0, 1) if mat[i][j + 1] != ' ' else complex(0, -1)
+    else:
+        new_dir = complex(1, 0) if mat[i + 1][j] != ' ' else complex(-1, 0)
+    return new_dir
+
+
+def solve(padded_mat):
+    start_j = padded_mat[1].index('|')
+    pos = complex(1, start_j)
+    direction = complex(1, 0)
+    letters = ''
+
+    while True:
+        i, j = _to_ints(pos)
+        m = padded_mat[i][j]
+        if m in ('|', '-'):
+            pass
+        elif m == '+':
+            direction = _new_direction(padded_mat, direction, i, j)
+        elif m == ' ':
+            return letters
+        else:
+            letters += m
+        pos += direction
+
+
+def main(fname):
+    mat = _parse(fname)
+    letters = solve(mat)
+    return letters
+
+
+if __name__ == '__main__':
+    assert 'ABCDEF' == main('./test.txt')
+    print(main('./input.txt'))

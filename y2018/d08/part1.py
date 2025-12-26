9baa48f4ebf148b2217f5a5a1ddb789f0b76e65c
Inon Peled <inon.peled@gmail.com>
2025-12-26 23:36:51 +0200

    y2018 d8 part1

---
+++ y2018/d08/part1.py
@@ -0,0 +1,33 @@
+def _parse(fname):
+    with open(fname) as f:
+        line = f.read()
+        nums = [int(e) for e in line.strip().split()]
+        return nums
+
+
+def solve(nums, start):
+    total = 0
+    num_children = nums[start]
+    num_metadata = nums[start + 1]
+    start += 2
+
+    for k in range(num_children):
+        t, e = solve(nums, start)
+        total += t
+        start = e
+
+    end = start + num_metadata
+    for j in range(start, end):
+        total += nums[j]
+    return total, end
+
+
+def main(fname):
+    nums = _parse(fname)
+    total, _ = solve(nums, 0)
+    return total
+
+
+if __name__ == '__main__':
+    assert 138 == main('./test.txt')
+    print(main('./input.txt'))

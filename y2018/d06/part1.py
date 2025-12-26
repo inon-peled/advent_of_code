3034decb460dcb0ecc71b9d706584d0d6dad5b72
Inon Peled <inon.peled@gmail.com>
2025-12-26 11:37:16 +0200

    y2018 d6 part1 solution idea

---
+++ y2018/d06/part1.py
@@ -0,0 +1,18 @@
+'''
+Solution idea:
+This is a Voronoi diagram in L1, not Euclidean distance. So:
+1. Form a bounding box.
+2. Do multi-source BFS from the data points, namely:
+2.1. Initialize the FIFO with all the data points.
+2.2. Continue BFS as usual, except that if a point was already visited, change its status to '.'.
+3. A data point has an infinitely large Voronoi cell if-and-only-if it lies on an edge of the bounding box.
+'''
+
+DATA = [(162, 168), (86, 253), (288, 359), (290, 219), (145, 343), (41, 301), (91, 214), (166, 260), (349, 353),
+        (178, 50), (56, 79), (273, 104), (173, 118), (165, 47), (284, 235), (153, 69), (116, 153), (276, 325),
+        (170, 58), (211, 328), (238, 346), (333, 299), (119, 328), (173, 289), (44, 223), (241, 161), (225, 159),
+        (266, 209), (293, 95), (89, 86), (281, 289), (50, 253), (75, 347), (298, 241), (88, 158), (40, 338), (291, 156),
+        (330, 88), (349, 289), (165, 102), (232, 131), (338, 191), (178, 335), (318, 107), (335, 339), (153, 156),
+        (88, 119), (163, 268), (159, 183), (162, 134)]
+TEST = [(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9)]
+
